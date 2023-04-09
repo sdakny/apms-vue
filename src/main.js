5f3c0c56813fa35c71dbba38c5986bbb07ca4f65
XIA WEIYI <sdakny@gmail.com>
2023-04-09 23:33:55 +0900

    Update login page and styles

--- src/main.js
+++ src/main.js
@@ -6,4 +6,7 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
+import '@/assets/styles/border.css'
+import '@/assets/styles/reset.css'
+
 createApp(App).use(store).use(router).use(ElementPlus).mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,11 @@ const routes = [
     component: Home
   },
   {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  },
+  {
     path: '/about',
     name: 'About',
     // route level code-splitting
